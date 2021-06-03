d06dd5bc998f63dc61625f30c911eba46427a7c0
Anonymous504 <60149294+Anonymous504@users.noreply.github.com>
2021-06-02 19:13:08 -0600

    ejercicio terminado

--- Ejercicios/convertidor/App.js
+++ Ejercicios/convertidor/App.js
@@ -1,9 +1,11 @@
+/* eslint-disable jsx-quotes */
+/* eslint-disable quotes */
 /* eslint-disable comma-dangle */
 /* eslint-disable semi */
 /* eslint-disable react/self-closing-comp */
 /* eslint-disable no-unused-vars */
 /* eslint-disable prettier/prettier */
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -13,9 +15,30 @@ import {
   TextInput,
   useColorScheme,
   View,
+  Button,
+  Alert,
+  Separator
 } from 'react-native';
 
+
 function App(){
+  const [entrada, setEntrada] = useState(0);
+  const [conversion, setConversion] = useState(0);
+
+  const showAlert = () => {
+    Alert.alert("Conversion Exitosa","Resultado " + conversion);
+  }
+
+  const convertir = ( valor ) => {
+    const resultado = valor * 24.6;
+    setConversion(resultado);
+  }
+
+  const inicializar = () =>{
+    setConversion(0);
+    setEntrada(0);
+
+  }
 
   return (
     <>
@@ -25,9 +48,22 @@ function App(){
       </View>
     </SafeAreaView>
     <View>
-      <TextInput style={styles.input} placeholder="Escriba Valor en Dolares"></TextInput>
-      <TextInput style={styles.input} placeholder="Escriba Valor en Lempiras"></TextInput>
-    </View>
+    <Text style={styles.sectionDescription}>Valor en Dolares</Text>
+      <TextInput
+      style={styles.input}
+      placeholder="Escriba Valor en Dolares"
+      keyboardType='numeric'
+      onChange={(e) => convertir(e.nativeEvent.text) }
+      defaultValue={entrada.toString()}></TextInput>
+
+      <Text style={styles.sectionDescription}>Valor en Lempiras</Text>
+      <TextInput
+      style={styles.input}
+      placeholder="Valor en Lempiras"
+      defaultValue={conversion.toString()}></TextInput>
+     <Button title="Calcular" color="#1E90FF" onPress={showAlert}>Calcular</Button>
+     <Button title="Limpiar" color="#1E90FF" onPress={inicializar}>Limpiar</Button>
+     </View>
     </>
 
   )
@@ -36,7 +72,7 @@ function App(){
 const styles = StyleSheet.create({
   encabezado:{
     backgroundColor: '#FFA000',
-    height: 200,
+    height: 150,
     borderBottomLeftRadius: 30,
     borderBottomRightRadius:30,
     alignItems: 'center'
@@ -46,6 +82,11 @@ const styles = StyleSheet.create({
       height: 40,
     margin: 12,
     borderWidth: 1,
+    },
+    separator: {
+      marginVertical: 8,
+      borderBottomColor: '#737373',
+      borderBottomWidth: StyleSheet.hairlineWidth,
     },
 
   sectionContainer: {
