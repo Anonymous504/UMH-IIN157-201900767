74021a6ac04b6e46a02adedc11939afa32533814
Anonymous504 <60149294+Anonymous504@users.noreply.github.com>
2021-06-10 19:16:58 -0600

    Examen Terminado

--- Examenes/examen2/App.js
+++ Examenes/examen2/App.js
@@ -17,26 +17,29 @@ import {
   View,
   Button,
   Alert,
-  Separator
+  Separator,
+  Image,
 } from 'react-native';
 
 
 function App(){
   const [entrada, setEntrada] = useState(0);
+  const [tasa, setTasa] = useState(0);
   const [conversion, setConversion] = useState(0);
 
   const showAlert = () => {
     Alert.alert("Conversion Exitosa","Resultado " + conversion);
   }
 
-  const convertir = ( valor ) => {
-    const resultado = valor * 24.6;
+  const convertir = () => {
+    const resultado = entrada * tasa;
     setConversion(resultado);
   }
 
   const inicializar = () =>{
-    setConversion(0);
     setEntrada(0);
+    setTasa(0);
+    setConversion(0);
 
   }
 
@@ -44,26 +47,37 @@ function App(){
     <>
     <SafeAreaView style={styles.encabezado}>
       <View>
-        <Text style={styles.sectionTitle}>Convertidor de Monedas</Text>
+        <Text style={styles.sectionTitle}>Convertidor de Divisas</Text>
         <Text style={styles.sectionTitle}>Joseph Fabricio Andino</Text>
       </View>
     </SafeAreaView>
-    <View>
-    <Text style={styles.sectionDescription}>Valor en Dolares</Text>
+    <View style={styles.container}>
+    <Image
+          style={styles.logo}
+          source={require("./convertir.png")}
+        />
+        </View>
+        <View>
+    <Text style={styles.sectionDescription}>Valor a Convertir</Text>
       <TextInput
       style={styles.input}
-      placeholder="Escriba Valor en Dolares"
+      placeholder="Escriba Valor a Convertir"
       defaultValue={entrada.toString()}
       keyboardType='numeric'
-      onChange={(e) => convertir(e.nativeEvent.text)}
+      value={entrada}
+      onChangeText={(newValue)=> setEntrada(newValue)}
       ></TextInput>
 
-      <Text style={styles.sectionDescription}>Valor en Lempiras</Text>
+      <Text style={styles.sectionDescription}>Tasa de Cambio</Text>
       <TextInput
       style={styles.input}
-      placeholder="Valor en Lempiras"
-      defaultValue={conversion.toString()}></TextInput>
-     <Button title="Calcular" color="#1E90FF" onPress={showAlert}>Calcular</Button>
+      placeholder="Escriba la Tasa de Cambio"
+      defaultValue={tasa.toString()}
+      keyboardType='numeric'
+      value={tasa}
+      onChangeText={(newValue)=> setTasa(newValue)}></TextInput>
+     <Button title="Calcular" color="#1E90FF" onPress={convertir}>Calcular</Button>
+     <Text style={styles.sectionTitle} alignItems='center'> Conversion: {conversion}</Text>
      <Button title="Limpiar" color="#1E90FF" onPress={inicializar}>Limpiar</Button>
      </View>
     </>
@@ -72,9 +86,20 @@ function App(){
 }
 
 const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  logo: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 100,
+    height: 100,
+},
   encabezado:{
-    backgroundColor: '#FFA000',
-    height: 150,
+    backgroundColor: '#40CAFF',
+    height: 130,
     borderBottomLeftRadius: 30,
     borderBottomRightRadius:30,
     alignItems: 'center'
